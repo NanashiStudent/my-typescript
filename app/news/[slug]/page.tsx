f047828b916ac5f1a69a4d00b1ef4c711d16b42d
NanashiStudent <183510465+NanashiStudent@users.noreply.github.com>
2024-10-29 01:12:28 +0900

    ニュースにISRを適用

--- app/news/[slug]/page.tsx
+++ app/news/[slug]/page.tsx
@@ -4,7 +4,7 @@ import Article from "@/app/_components/Article";
 import ButtonLink from "@/app/_components/ButtonLink";
 import { notFound } from "next/navigation";
 
-export const revalidate = 0;
+export const revalidate = 60;
 
 type Props = {
   params: {
